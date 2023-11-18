0f2c6b4cd7d3d2220c3568941371ebb5b552f108
Surya-Vamshi <p.suryavamshi5@gmail.com>
2023-11-18 21:25:43 +0100

    Added two Controllers and Fixed ingredients bug in Recipes.

--- backend/routes/testRoutes.js
+++ backend/routes/testRoutes.js
@@ -5,8 +5,8 @@ const recipeBuilder = require('../controllers/recipeController');
 module.exports = app => {
     app.route('/allergies').get(allergyBuilder.list_all_allergies);
 
-    app.route('/ingredients/:allergies/:lifestyle').get(ingredientBuilder.read_allowed_ingredients);
-    app.route('/ingredients/:ingredients').update(ingredientBuilder.updated_disliked_ingredients);
+    app.route('/get_ingredients').post(ingredientBuilder.read_allowed_ingredients);
+    app.route('/disliked_ingredients').post(ingredientBuilder.updated_disliked_ingredients);
 
-    app.route('/recipes/:allergies/:lifestyle').get(recipeBuilder.read_allowed_recipes);
+    app.route('/recipes').post(recipeBuilder.read_allowed_recipes);
 };
