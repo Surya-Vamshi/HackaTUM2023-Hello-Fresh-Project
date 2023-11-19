ab038e79674235da56297bfd11200b8b95d1674e
Surya-Vamshi <p.suryavamshi5@gmail.com>
2023-11-19 07:34:18 +0100

    Added images to Ingredients page

--- frontend/src/components/Dislike-Personalisation.vue
+++ frontend/src/components/Dislike-Personalisation.vue
@@ -3,15 +3,21 @@
     <h1 class="title">Which ingredients can you not stand?</h1>
     <b-form-group>
       <b-row class="diet-options">
-        <b-col v-for="ingredient in allIngredients" v-bind:key="ingredient._id" sm="2" class="color-circle">
-          <b-form-checkbox inline
-                        v-model="selectedIngredients"
-                        :key="ingredient.name"
-                        :value="ingredient._id"
-          >
-            {{ ingredient.name }}
-          </b-form-checkbox>
+        <b-col v-for="ingredient in allIngredients" :key="ingredient._id" sm="2"
+               :class="['color-circle', selectedIngredients.includes(ingredient._id) ? 'selected' : '']">
+          <!-- Invisible checkbox -->
+          <input type="checkbox"
+                 :id="`checkbox-${ingredient._id}`"
+                 v-model="selectedIngredients"
+                 :value="ingredient._id"
+                 class="invisible-checkbox" />
+          <!-- Custom label which controls the checkbox -->
+          <label :for="`checkbox-${ingredient._id}`" class="ingredient-label">
+            <img :src="ingredient.imageLink" :alt="ingredient.name" class="ingredient-image">
+            <span>{{ ingredient.name }}</span>
+          </label>
         </b-col>
+
       </b-row>
     </b-form-group>
     <b-button class="button" @click="goToNextPersonalisationPage()">Next</b-button>
@@ -54,13 +60,11 @@ export default {
   methods: {
     goToNextPersonalisationPage() {
       // console.log(this.selectedIngredients);
-      let newlist = [];
-      this.allIngredients.forEach(ingredient => {
-        newlist.push(ingredient._id);
-      });
-      this.$emit('getRecipes', newlist, this.selectedIngredients);
-
-    }
+      // this.$emit('nextPage', 'lifestyle', this.selectedAllergies);
+    },
+    isSelected(ingredientId) {
+      return this.selectedIngredients.includes(ingredientId);
+    },
   }
 }
 </script>
@@ -98,11 +102,12 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 150px;
-  height: 150px;
+  width: 125px;
+  height: 125px;
   background-color: #effff4; /* Light background color similar to HelloFresh theme */
   margin-bottom: 0.5rem;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  transition: background-color 0.2s; /* Smooth transition for background color change */
 
 }
 
@@ -117,5 +122,27 @@ export default {
   display: flex;
   justify-content: space-around;
   align-items: center;
+}
+
+.ingredient-image {
+  width: 75px; /* Adjust size as needed */
+  height: 75px; /* Adjust size as needed */
+  object-fit: cover; /* Ensures the image covers the area without stretching */
+  border-radius: 50%; /* Circular images */
+  margin-bottom: 10px; /* Space between image and checkbox */
+}
+
+.invisible-checkbox {
+  opacity: 0;
+  position: absolute;
+  width: 0;
+  height: 0;
+  margin: 0;
+  padding: 0;
+}
+
+.color-circle.selected {
+  background-color: #c8e6c9; /* New background color when selected */
+  transform: scale(1.1); /* Optional: scale up the circle slightly when selected */
 }
 </style>
